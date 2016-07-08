69dc8563ebcaa1142c235fe99da0ca3e4c221179
himanshu_aggarwal <himanshu.aggarwal@reltio.com>
2016-07-08 19:10:24 +0530

    Changes

--- app.js
+++ app.js
@@ -8,20 +8,37 @@ var db = require('./model/db');
 var sf = require('./lib/salesforce');
 var config = require('./config/config');
 var mongoObj = require('./config/monogobjects.json');
+var mongodb = require('mongodb');
+var databs;
 
 var routes = require('./routes/index');
 /*var users = require('./routes/users');*/
 
 var app = express();
 
+//Initialize Mongoose
+console.log('config.mongourl '+config.mongourl);
+mongodb.MongoClient.connect(config.mongourl, function(error, db){
+    if(error) {
+       console.log(error);
+        //process.exit(1)
+    }
+    databs = db;
+    app.set('database',databs);
+    console.log('successfully connected to '+config.mongourl);
+});
+
+
+
+
 //Initialize sf
-sf.init(config,function(res){
-  console.log('Initialized Sf..');
-  app.set('sf',sf);
+sf.init(config, function (res) {
+    console.log('Initialized Sf..');
+    app.set('sf', sf);
 })
 
-app.set('salesmongoconfig',config);
-app.set('mongoObj',mongoObj);
+app.set('salesmongoconfig', config);
+app.set('mongoObj', mongoObj);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'public'));
@@ -30,29 +47,29 @@ app.set('views', path.join(__dirname, 'public'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
-app.use('/js',express.static(path.join(__dirname, 'public/js')));
-app.use('/js',express.static(path.join(__dirname, 'node_modules/angular-route')));
-app.use('/js',express.static(path.join(__dirname, 'node_modules/angular')));
-app.use('/css',express.static(path.join(__dirname, 'public/stylesheets')));
-app.use('/css',express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, 'public/js')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules/angular-route')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules/angular')));
+app.use('/css', express.static(path.join(__dirname, 'public/stylesheets')));
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
 app.use(express.static(path.join(__dirname, 'public')));
 // uncomment after placing your favicon in /public
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 //app.use(cookieParser());
 
 app.use('/api', routes);
 
-app.use('*', function(req, res){
-  res.render('index.html');
+app.use('*', function (req, res) {
+    res.render('index.html');
 });
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -60,23 +77,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 
--- routes/index.js
+++ routes/index.js
@@ -1,42 +1,105 @@
 var express = require('express');
 var router = express.Router();
 var sf = require('./../lib/salesforce');
+var mongodb = require('mongodb');
+var ObjectID = mongodb.ObjectID;
 
 /* GET home page. */
 /*
-router.get('*', function(req, res) {
-  res.renderFile('./views/index.html'); // load the single view file (angular will handle the page changes on the front-end)
-});
-*//*
-router.get('*', function(req, res, next) {
-  res.render('index');
-});*/
+ router.get('*', function(req, res) {
+ res.renderFile('./views/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+ });
+ *//*
+ router.get('*', function(req, res, next) {
+ res.render('index');
+ });*/
 
-router.get('/', function(req, res, next) {
-  res.send('respond with a api resource');
-});
-
-router.get('/salesObject', function(req, res,next) { //isthe next important here? I dont think so because we are usinf send but plz check
-  var sf = res.app.get('sf');
-  sf.getSObejctTypes(function(result) {
-    res.status(200).send(result);
-  },function(err) {
-    console.log(err)
-  })
+router.get('/', function (req, res, next) {
+    res.send('respond with a api resource');
 });
 
-router.get('/mongoObject', function(req, res, next) {
-  var mongoObj = res.app.get('mongoObj');
-  var mObj = [];
-  for(var key in mongoObj) {
-    mObj.push(key);
-  }
-  res.status(200).send(mObj);
+router.get('/salesObjects', function (req, res, next) { //isthe next important here? I dont think so because we are usinf send but plz check
+    var sf = res.app.get('sf');
+    sf.getSObejctTypes(function (result) {
+        res.status(200).send(result);
+    }, function (err) {
+        console.log(err)
+    })
 });
 
-router.get('/config', function(req, res, next) {
-  var sfconfig = res.app.get('salesmongoconfig');
-  res.status(200).send(sfconfig);
+router.get('/mongoObjects', function (req, res, next) {
+    var mongoObj = res.app.get('mongoObj');
+    var mObj = [];
+    for (var key in mongoObj) {
+        mObj.push(key);
+    }
+    res.status(200).send(mObj);
 });
 
+router.get('/mongoObjectsIds', function (req, res, next) {
+    var mongoObj = res.app.get('mongoObj');
+    res.status(200).send(mongoObj);
+});
+
+router.get('/config', function (req, res, next) {
+    var sfconfig = res.app.get('salesmongoconfig');
+    res.status(200).send(sfconfig);
+});
+
+router.get('/salesObjects/:objectName/attributes', function (req, res, next) { //isthe next important here? I dont think so because we are usinf send but plz check
+    var sf = res.app.get('sf');
+    var objName = req.params.objectName;
+    sf.getSObjectAttribs(objName, function (result) {
+        res.status(200).send(result);
+    }, function (err) {
+        console.log(err)
+    })
+});
+
+router.get('/mongoObjects/:objectName/:objId', function (req, res, next) { //isthe next important here? I dont think so because we are usinf send but plz check
+    var db = res.app.get('database');
+    var objName = req.params.objectName;
+    var objId = req.params.objId;
+
+    db.collection(objName).find({_id: new ObjectID(objId)}).toArray(function (err, data) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.status(200).json(data[0]);
+        }
+    })
+
+});
+
+router.post('/attibutes', function (req, res, next) {
+    var body = req.body;
+    console.log('body is ' + JSON.stringify(body))
+    for (var i = 0; i < body.length; i++) {
+        var temp = body[i];
+        var salesTemp = temp.salesObjects[0];
+        var mongoTemp = temp.mongoObjects[0];
+        expresSales.mongoObjId().then(function (data) {
+            console.log('data is ' + JSON.stringify(data.data));
+            if (data.data[mongoTemp]) {
+                console.log('data.data[mongoTemp] ' + data.data[mongoTemp])
+                var p1 = expresSales.mongoObjAttr(mongoTemp, data.data[mongoTemp]);
+                var p2 = expresSales.salesObjAttr(salesTemp);
+                $q.all([p1, p2]).then(function (result) {
+                    console.log('after p1 and p2 is '+JSON.stringify(result));
+                }, function (error) {
+                    console.log(error);
+                })
+            } else {
+                console.log('No match')
+            }
+
+
+        }, function (error) {
+
+        })
+    }
+    res.status(200).send('Hello , i m post ')
+
+    function
+});
 module.exports = router;
